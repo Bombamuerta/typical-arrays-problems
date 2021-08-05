33e1b880ce81f45afbea053f2f741d02600f90e7
Farber Denis <denis.farber@instatsport.com>
2021-08-05 13:33:19 +0300

    feat: solution added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array) {
+    if (array && !undefined && arguments && array.length != 0) {
+        let minResult;
+        array.sort((a, b) => a - b);
+        return (minResult = array[0]);
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array && !undefined && arguments && array.length != 0) {
+        let maxResult;
+        array.sort((a, b) => b - a);
+        return (maxResult = array[0]);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array && !undefined && arguments) {
+        return array.reduce((sum, cur) => {
+            return Math.round((sum + cur / array.length) * 100) / 100;
+        }, 0);
+    } else {
+        return 0;
+    }
+};
